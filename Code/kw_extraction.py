c07cea7506679f4bb735853889ca05a47adc2ced
Nayaeun <110697102+Nayaeun@users.noreply.github.com>
2023-12-07 16:38:31 -0500

    make class for kw extraction

--- Code/kw_extraction.py
+++ Code/kw_extraction.py
@@ -36,20 +36,37 @@ from keybert import KeyBERT
 import requests
 from bs4 import BeautifulSoup
 
-def fetch_text_from_url(url):
-    # Fetch the text content from the URL using requests and BeautifulSoup
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    # Extract text from the webpage (adjust based on the structure of the webpage)
-    text = ' '.join([p.get_text() for p in soup.find_all('p')])
-    return text
+class KeywordExtractor:
+    def __init__(self):
+        self.kw_extractor = KeyBERT()
+
+    def extract_keywords(self, input_data, is_url=True):
+        if is_url:
+            text = self.fetch_text_from_url(input_data)
+        else:
+            text = input_data
+
+        # Use the extract_keywords function from the KeyBERT library
+        keywords = self.kw_extractor.extract_keywords(text, keyphrase_ngram_range=(1, 1), stop_words='english')
+        return keywords
+
+    def fetch_text_from_url(self, url):
+        # Fetch the text content from the URL using requests and BeautifulSoup
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        # Extract text from the webpage (adjust based on the structure of the webpage)
+        text = ' '.join([p.get_text() for p in soup.find_all('p')])
+        return text
 
 # Example usage
+keyword_extractor = KeywordExtractor()
+
+# Example 1: Extract keywords from text
+text = "Keyword extraction is a valuable task in natural language processing."
+keywords_text = keyword_extractor.extract_keywords(text, is_url=False)
+print("Keywords from text:", keywords_text)
+
+# Example 2: Extract keywords from URL
 url = 'https://www.infoq.com/news/2023/12/aws-titan-image-generator/'
-text_from_url = fetch_text_from_url(url)
-
-kw_extractor = KeyBERT()
-
-# Use the extract_keywords function from the previous example
-keywords = kw_extractor.extract_keywords(text_from_url, keyphrase_ngram_range=(1, 1), stop_words='english')
-print("Keywords:", keywords)
+keywords_url = keyword_extractor.extract_keywords(url)
+print("Keywords from URL:", keywords_url)

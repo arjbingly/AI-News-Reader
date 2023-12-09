a418babff680f7e5b55bc5afea0f51345966f228
Arjun Bingly <arjunbin@gmail.com>
2023-12-09 02:12:46 -0500

    Langchain for summariser BETA

--- Code/summarizer.py
+++ Code/summarizer.py
@@ -3,6 +3,7 @@ import requests
 from transformers import pipeline
 from transformers import AutoTokenizer, BartForConditionalGeneration
 from news_fetch import NewsArticle
+import torch
 class Summarizer:
     def __init__(self):
         self.model_name = "facebook/bart-large-cnn"
@@ -10,8 +11,10 @@ class Summarizer:
         self.model = BartForConditionalGeneration.from_pretrained(self.model_name)
         self.max_length = 500
         self.min_length = 100
+        self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
+        self.model = self.model.to(self.device)
     def summarization(self, text):
-        inputs = self.tokenizer(text, max_length=self.max_length, return_tensors="pt", truncation=True)
+        inputs = self.tokenizer(text, return_tensors="pt", truncation=True).to(self.device)
         input_ids = inputs.input_ids
         summary_ids = self.model.generate(input_ids, max_length=self.max_length, min_length=self.min_length, do_sample=False)
         summary = self.tokenizer.decode(summary_ids[0], skip_special_tokens=True, clean_up_tokenization_spaces=False)

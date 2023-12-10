cdffbecc2997af75c70df20bec38cd85f2495a87
Arjun Bingly <arjunbin@gmail.com>
2023-12-10 16:06:35 -0500

    Update app.py

--- Code/app.py
+++ Code/app.py
@@ -1,11 +1,14 @@
 # To Run: streamlit run app.py --server.port=8888
 import streamlit as st
+import pandas as pd
 
 from news_fetch import NewsArticle
-from summarizer import Summarizer
+from zeroshot import ZeroShotClassifier
 from kw_extraction import KeywordExtractor
-from QnA import QuestionAnswering
+from chatbot import ConversationalQA
+from summarizer import Summarizer
 
+from streamlit_tags import st_tags
 #%%
 st.set_page_config(
     page_title="SMART News Reader",
@@ -45,61 +48,122 @@ with st.expander("🚀 How to Use", expanded=False):
         """
     )
 #%%
-
+def clear_chat():
+    if 'chatbot' in st.session_state:
+        st.session_state.chatbot.clear_mem()
 
 with st.sidebar:
-    url_input = st.text_input('Enter the News Article URL',
-                  )
-    do_summarization = st.toggle(label='Summarization',
-                            value='True')
+    st.text_input('Enter the News Article URL',
+                  value = 'https://www.cnn.com/2023/10/03/europe/nobel-prize-physics-electrons-flashes-light-intl-scn/index.html',
+                  key='url_input',
+                  on_change = clear_chat())
+    st.toggle(label='Summarization',
+              value='True',
+              key = 'do_summarization')
 
-    do_key_word = st.toggle(label='Key Word Extractor',
-                            value = 'True')
-    if do_key_word:
-        min_Ngrams = st.slider("Minimum Ngram", min_value=1, max_value=4, value=1, step = 1)
-        max_Ngrams = st.slider("Maximum Ngram", min_value=1, max_value=4, value=1, step = 1)
-        if min_Ngrams > max_Ngrams:
+    st.toggle(label='Key Word Extractor',
+                            value = 'True',
+                            key='do_key_word')
+    if st.session_state.do_key_word:
+        st.slider("Minimum Ngram", min_value=1, max_value=4, value=1, step = 1, key='min_Ngrams')
+        st.slider("Maximum Ngram", min_value=1, max_value=4, value=1, step = 1, key='max_Ngrams')
+        if st.session_state.min_Ngrams > st.session_state.max_Ngrams:
             st.write(":red[Min. Ngram can't be greater than Max. Ngram]")
-            max_Ngrams = min_Ngrams
+            st.session_state.max_Ngrams = st.session_state.min_Ngrams
 
-    do_qna = st.toggle(label='Question Answering',
-                            value='True')
+    st.toggle(label='Zero-Shot Classifier',
+              value='True',
+              key='do_zeroshot')
 
-if not url_input:
-    url_input = 'https://www.cnn.com/2023/10/03/europe/nobel-prize-physics-electrons-flashes-light-intl-scn/index.html'
+    st.toggle(label='Question Answering',
+              value='True',
+              key='do_qna')
 
-news = NewsArticle(url_input)
+
 #%%
+# Load models
+@st.cache_resource
+def load_summarizer():
+    return Summarizer()
+
+@st.cache_resource
+def load_chatbot():
+    return ConversationalQA()
+
+@st.cache_resource
+def load_keyword_extractor(min_Ngrams, max_Ngrams):
+    return KeywordExtractor(ngram_range=(min_Ngrams, max_Ngrams))
+
+@st.cache_resource
+def load_zeroshot():
+    return ZeroShotClassifier()
+
+
+#%%
+news = NewsArticle(st.session_state.url_input)
+lang = news.article.meta_lang
 st.header(news.article.title)
 st.subheader(' ,'.join(news.article.authors))
 st.caption(f'Source: {news.article.url} ')
 st.caption(f'Publish Date: {news.article.publish_date}')
 st.image(news.article.top_image)
+
+
 #%%
-if do_key_word:
+if st.session_state.do_key_word:
     st.divider()
     st.subheader('Key Words')
-    keyword_extractor = KeywordExtractor(ngram_range=(min_Ngrams, max_Ngrams))
-    st.table(keyword_extractor.extract_keywords(news.article.text))
+    keyword_extractor = load_keyword_extractor(st.session_state.min_Ngrams, st.session_state.max_Ngrams)
+    key_words_df = pd.DataFrame(keyword_extractor.extract_keywords(news.article.text),
+                                columns=['Key Word', 'Score'])
+    col1, col2, col3 = st.columns([1,2,1])
+    col2.dataframe(key_words_df, hide_index=True, use_container_width=True)
 #%%
-if do_summarization:
+if st.session_state.do_zeroshot:
     st.divider()
-    summarizer = Summarizer()
+    st.subheader('Zero-Shot Classifier')
+    classifier = load_zeroshot()
+    keywords = st_tags(label='',
+                       text='Press enter to add more',
+                       value= ['Politics', 'Science', 'Business', 'Travel'])
+    zeroshot_results = classifier.classify(news.article.text, keywords)
+    zeroshot_df = pd.DataFrame(classifier.classify(news.article.text, keywords))
+    zeroshot_df = zeroshot_df.drop(columns='sequence')
+    zeroshot_df = zeroshot_df.sort_values('scores', ascending=False)
+    # zeroshot_df = zeroshot_df.sort_values('Probability').reset_index()
+    col1, col2, col3 = st.columns([1,2,1])
+    col2.dataframe(zeroshot_df, hide_index=True, use_container_width=True)
+#%%
+if st.session_state.do_summarization:
+    st.divider()
+    summarizer = load_summarizer()
     st.subheader('Summary')
-    summary = summarizer.summarization(news.article.text)
+    summary = summarizer.summarize(news.article.text)
     st.write(summary)
     # st.write(news.article.text)
 #%%
-if do_qna:
+
+
+if st.session_state.do_qna:
     st.divider()
-    qna = QuestionAnswering()
+    st.session_state.chatbot = load_chatbot()
+    st.session_state.chatbot.create_vector_db(news.article.text)
+    st.session_state.chatbot.create_pipe()
     st.subheader('Question Answering about Article')
+
+    for message in st.session_state.chatbot.memory.chat_memory.messages:
+        with st.chat_message(message.type):
+            st.write(message.content)
+
+
     question = st.chat_input("Ask me questions from the article..")
     if question:
-        with st.chat_message('user'):
+        with st.chat_message('human'):
             # st.write(f':green[*USER* ---> {question}]')
             st.write(question)
+        result = st.session_state.chatbot.infer(question)
         with st.chat_message('ai'):
             # st.write(f':blue[*ANSWER* --> {qna.infer(question, news.article.text)}]')
-            st.write(qna.infer(question, news.article.text))
-    # st.write(news.article.text)
+            st.write(result['answer'])
+
+    st.button('Clear Chat', on_click=clear_chat())
